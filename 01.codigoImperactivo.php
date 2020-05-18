d9c8c653caa93fa51efb9cd1f1757070128bbab4
anapetro <48393206+anapetro@users.noreply.github.com>
2020-05-17 23:44:24 -0500

    POO

--- 01.codigoImperactivo.php
+++ 01.codigoImperactivo.php
@@ -1,8 +1,8 @@
 <?php
 # codigo imperativo o espagueti 
 
-$automovil1 = (objeto)["marca"=> "toyota", "modelo"=> " 4Runner "];
-$automovil2 = (objeto)["marca"=> "Jeep", "modelo"=> "Renegade"];
+$automovil1 = (object)["marca"=> "toyota", "modelo"=> " 4Runner "];
+$automovil2 = (object)["marca"=> "Jeep", "modelo"=> "Renegade"];
 
 function mostrar($automovil){
     echo "<p>hola!soy un $automovil->marca, modelo $automovil->modelo</p>";
